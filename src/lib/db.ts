3e1d135f0acdb4dbc4669811cd1e3ce71fb3c707
Steffen Tveit <steffentveit@outlook.com>
2025-11-28 18:22:04 +0100

    Added message system for user input and ai output

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma/client'
+import { PrismaClient } from '@/generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = global as unknown as {
@@ -9,10 +9,8 @@ const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 })
 
-const prisma = globalForPrisma.prisma || new PrismaClient({
+export const prisma = globalForPrisma.prisma || new PrismaClient({
   adapter,
 })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export default prisma
