17959fe0755851b666c1fc474e6685a08c83d51f
Steffen Tveit <steffentveit@outlook.com>
2025-12-03 13:54:37 +0100

    Added fragment view on project page with several control buttons

--- src/modules/projects/ui/components/message-loading.tsx
+++ src/modules/projects/ui/components/message-loading.tsx
@@ -45,7 +45,7 @@ export const MessageLoading = () => {
           height={18}
           className="shrink-0"
         />
-        <span className="text-sm font-medium"></span>
+        <span className="text-sm font-medium">Weaver</span>
       </div>
       <div className="pl-8.5 flex flex-col gap-y-4">
         <ShimmerMessages />
